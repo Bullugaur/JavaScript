d524d96f9b4207d1a81358c0a058fe7c2ab59781
Jyoti gaur <112107175+Bullugaur@users.noreply.github.com>
2024-08-06 17:04:16 +0000

    DataTypes

---
+++ 01_basics/01_dataType.js
@@ -0,0 +1,27 @@
+"use strict"; //treate all code of javaScript as a newer version
+
+// alert(3+3) // we are using nodejs not browser
+
+console.log(3
+    +
+3  )  // code readbility should be high
+
+let name="bulbul"
+let age=18
+let isLoggedIn=true
+
+  /* Data Type.............
+number
+bigint
+string => ""
+boolean => true/false
+null = standalone value => let state=null;
+undefind => let state;
+symbol => unique */
+
+//objects
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+
