067993d3ab1903b329fbc0ade77a15108a361cb4
Jyoti gaur <112107175+Bullugaur@users.noreply.github.com>
2024-08-08 17:33:39 +0000

    Oprations

--- 01_basics/03_conversionOpration.js
+++ 01_basics/03_conversionOpration.js
@@ -1,11 +1,13 @@
 let score="33abcd"
 
-console.log(typeof score)
-console.log(typeof (score));
+// console.log(typeof score)
+// console.log(typeof (score));
 
 let numberId=Number (score);
-console.log(typeof numberId);
-console.log(numberId);
+// console.log(typeof numberId);
+// console.log(numberId);
+
+// *** Notes About number conversion *** //
 
 // "33" => 33
 // "33ab" => NaN
@@ -15,8 +17,43 @@ console.log(numberId);
 let isLogged="a"
 
 let boolenId=Boolean(isLogged);
-console.log(boolenId);
+// console.log(boolenId);
+
+
+// *** Notes About Boolen conversion *** //
 
 // 1 => true; 0 => false;
 // "" => false
 // "value" => true
+
+
+
+// ******************** Oprations ****************** //
+
+let value=3
+let negValue= -value
+console.log(negValue);
+
+// Some Basic Oprations //
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1="heloo "
+let str2="Bulbul"
+
+let str3=str1+str2
+// console.log(str3);
+
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+3);
+// console.log(1+2+"3");
+
+
+
+
