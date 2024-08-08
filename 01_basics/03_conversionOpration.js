9611dca758fa19b065d8b327e7d7d0863bec9789
Jyoti gaur <112107175+Bullugaur@users.noreply.github.com>
2024-08-08 17:09:02 +0000

    conversion

---
+++ 01_basics/03_conversionOpration.js
@@ -0,0 +1,22 @@
+let score="33abcd"
+
+console.log(typeof score)
+console.log(typeof (score));
+
+let numberId=Number (score);
+console.log(typeof numberId);
+console.log(numberId);
+
+// "33" => 33
+// "33ab" => NaN
+// true => 1; false => 0;
+
+
+let isLogged="a"
+
+let boolenId=Boolean(isLogged);
+console.log(boolenId);
+
+// 1 => true; 0 => false;
+// "" => false
+// "value" => true
