6fd6ee0b5201399fcc3d01f0ada5e4cffba50e61
Jyoti gaur <112107175+Bullugaur@users.noreply.github.com>
2024-08-08 18:05:57 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,27 @@
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 > 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// **** Avoid all these comparisions from line 10 to 18 and write clean code **** //
+
+console.log("2" > 0); // possible but not write such this type of compairision
+
+console.log(null > 0);
+console.log(null >= 0);
+console.log(null == 0); // all three compairison are possible but not write in such un professionaliy code
+
+
+console.log(undefined == 0);
+console.log(undefined >=0);// same rule follow here....
+
+
+
+// === "strict comapirison "
+// Because check not value only also checks dataType
+
+console.log(2 === 2);// true
+console.log("2" === 2);// false
+
