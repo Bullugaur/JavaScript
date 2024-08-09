2cdec518cc361fb0bbe7d157007464cc4c94b755
Jyoti gaur <112107175+Bullugaur@users.noreply.github.com>
2024-08-09 05:52:47 +0000

    summary

---
+++ 01_basics/DataType_Summery.js
@@ -0,0 +1,42 @@
+// we have two type of data type
+ 
+// PRIMITIVE 
+let number=12           // number
+let string="abc"       // string
+let boolen=true/false // boolean
+let nul=null         // null
+let numId;           // undefined
+let bigNo=123457543234565 // bigInt
+let value=Symbol("abc") // symbol
+
+
+// NON PRIMITIVE ( REFRENCE )
+
+let heros=["ram","shyam","ect"]// array
+let MyDetail={
+    name:"jyoti",
+    age:18
+} // object
+
+let add= function(a){
+    return a+a;
+} // fuction 
+
+// ******* return type of all dataType ******** //
+
+// **** Primitive Datatypes **** //
+
+// Number => number
+// String  => string
+// Boolean  => boolean
+// null  => object
+// undefined  =>  undefined
+// Symbol  =>  symbol
+// BigInt  =>  bigint
+
+
+// **** Non-primitive Datatypes **** //
+
+// Arrays  =>  object
+// Function  =>  function
+// Object  =>  object
