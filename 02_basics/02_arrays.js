38625bf2f6f158fe97fb226162f2f212964e1ec3
Jyoti gaur <112107175+Bullugaur@users.noreply.github.com>
2024-08-13 17:00:47 +0000

    array2

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,28 @@
+
+const marval_heros=["thor","ironman","spiiderman"]
+const dc_heros=["superman","flash","batman"]
+
+// marval_heros.push(dc_heros);
+// console.log(marval_heros);
+
+// const allHeros=marval_heros.concat(dc_heros);
+// console.log(allHeros);
+
+// const all_newHeros=[...marval_heros,...dc_heros]
+// console.log(all_newHeros);
+
+// const another_array=[1,2,3,[4,5,6],7,[6,7,[4,5]]]
+// const real_anotherArr=another_array.flat(2)
+// console.log(real_anotherArr);
+
+console.log(Array.isArray("bulbul"));
+console.log(Array.from("bulbul"));
+console.log(Array.from({name:"bulbul"})) // intersting
+
+let score1=100
+let score2=200
+let score3=300
+
+console.log(Array.of(score1,score2,score3));
+
+
